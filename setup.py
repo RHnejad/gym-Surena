0148ecaf2442489cbf677930b210e6b6c13baa53
Reyhaneh Hosseininejad <30596230+RHnejad@users.noreply.github.com>
2021-09-11 18:16:27 +0430

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Surena",
+    version='0.0.1',
+    install_requires=['gym',
+                      'pybullet',
+                      'numpy',
+                      'time',
+                      'pybullet_data']
+)
